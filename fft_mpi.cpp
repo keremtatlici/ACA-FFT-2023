3971ea7c022fed5106692b0dc7a79c6b4a77f848
Kerem Tatlıcı <33085629+keremtatlici@users.noreply.github.com>
2023-01-07 15:51:12 +0100

    init

---
+++ fft_mpi.cpp
@@ -0,0 +1,36 @@
+
+void fft1d()
+{
+
+}
+
+
+void fft2d()
+{
+
+}
+
+
+void remove_coefficient()
+{
+
+}
+
+
+void ifft1d()
+{
+
+}
+
+void ifft2d()
+{
+
+}
+
+
+
+//main method
+int main(int argc, char *argv[])
+{
+
+}
