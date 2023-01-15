a3a82d6bef3c63bdc088b10144605917ef7d493e
Kerem Tatlıcı <33085629+keremtatlici@users.noreply.github.com>
2023-01-15 13:03:24 +0100

    installation steps added as a bash script for vm clusters

---
+++ install_vm.sh
@@ -0,0 +1,13 @@
+sudo apt install g++ gcc build-essential htop wget -y
+wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.1.tar.gz
+tar -xzvf openmpi-4.1.1.tar.gz
+cd openmpi-4.1.1/
+./configure --prefix=/usr/local/openmpi-4.1.1
+sudo make -j$(nproc)
+sudo make install
+
+# write me script that removes below lines from .bashrc
+#MPI_HOME=/usr/local/openmpi-4.1.1
+#PATH=$MPI_HOME/bin:$PATH
+
+
